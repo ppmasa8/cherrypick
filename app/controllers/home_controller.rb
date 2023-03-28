b368dafda692d591266d305fd4b842655b973beb
ppmasa8 <kiyokiyopip56@gmail.com>
2023-03-29 07:11:20 +0900

    added form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
     def index
+        @text = params[:text]
+    end
+
+    def post
+        render("home/index") and return if params[:text].nil?
+
+        @text = params[:text]
+        redirect_to action: 'index', text: @text
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
+  get "home/index" => "home#index"
+  post "home/post" => "home#post"
+  get "home/post" => "home#post"
 end
