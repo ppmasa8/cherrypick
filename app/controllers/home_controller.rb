b64afaa30b63a6021bbddf8ec1b7faf75aca5efd
ppmasa8 <kiyokiyopip56@gmail.com>
2023-04-01 09:41:02 +0900

    improve controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,28 @@
+require 'extract'
+
 class HomeController < ApplicationController
+    before_action :set_text, only: [:index, :new]
+
     def index
-        @text = params[:text]
     end
 
-    def post
-        render("home/index") and return if params[:text].nil?
+    def new
+        if @text.nil?
+            flash[:alert] = "Please input text."
+            render("home/index") and return
+        end
 
-        @text = params[:text]
+        flash[:notice] = "Text was successfully updated."
         redirect_to action: 'index', text: @text
+    end
+
+    private
+
+    def set_text
+        @text = home_params[:text]
+    end
+
+    def home_params
+        params.permit(:text)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "home#index"
   get "home/index" => "home#index"
-  post "home/post" => "home#post"
-  get "home/post" => "home#post"
+  post "home/new" => "home#new"
+  get "home/new" => "home#new"
+  root "home#index"
 end
