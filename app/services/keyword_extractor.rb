cb8faf4a2a5f7d2b2f2c0aa2dfe48d1649a49e3b
ppmasa8 <kiyokiyopip56@gmail.com>
2023-04-01 11:14:59 +0900

    added rules about extract_keywords

--- app/services/keyword_extractor.rb
+++ app/services/keyword_extractor.rb
@@ -7,7 +7,7 @@ class KeywordExtractor
 
     def extract_keywords(text)
         keywords = []
-        @suika_tagger.parse(text).each { |word| keywords << word }
+        @suika_tagger.parse(text).each { |word| keywords << word if word.include?("名詞") || word.include?("形容詞") }
         keywords
     end
 end
