78071ac471a8dcb28dfcbec9313aba079bcf550d
ppmasa8 <kiyokiyopip56@gmail.com>
2023-04-05 22:28:04 +0900

    added rspec

---
+++ spec/services/translate_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Translate do
+    describe '#deepl_translate' do
+        it 'returns the translated text' do
+            text = '私は言語AIで、あなたの質問に答えることができます。'
+            source_lang, translated_text = Translate.new.deepl_translate(text)
+            expect(translated_text).to eq('I am a language AI and can answer your questions.')
+        end
+
+        it 'returns the chopped up translated text' do
+            text = '空、自由、タケコプター、寒い、広い'
+            source_lang, translated_text = Translate.new.deepl_translate(text)
+            expect(translated_text).to eq('Sky, freedom, bamboo shoots, cold, wide')
+        end
+    end
+end
